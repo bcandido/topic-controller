5ba56665532d1f9adf93a9af08a41ac2087730d6
Brenno Candido <bcandido.bc@gmail.com>
2020-07-05 08:23:53 -0300

    minor --  rand string

--- rand/rand.go
+++ rand/rand.go
@@ -5,18 +5,16 @@ import (
 	"time"
 )
 
-const charset = "abcdefghijklmnopqrstuvwxyz" +
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const charset = "abcdefghijklmnopqrstuvwxyz" + "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-var seededRand *rand.Rand = rand.New(
-	rand.NewSource(time.Now().UnixNano()))
+var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func StringWithCharset(length int, charset string) string {
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
+	generatedString := make([]byte, length)
+	for i := range generatedString {
+		generatedString[i] = charset[seededRand.Intn(len(charset))]
 	}
-	return string(b)
+	return string(generatedString)
 }
 
 func String(length int) string {
